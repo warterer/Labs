88892b255c44edb7394d06cc470dd5ad4677d464
areutskyi <artemik.tamik@gmail.com>
2024-12-13 20:37:01 +0200

    removeElement upd

--- Lab7/script.js
+++ Lab7/script.js
@@ -1,9 +1,11 @@
-const removeElement = (array, item) => {
-    const index = array.indexOf(item);
-    if (index !== -1)
-    {
-        array.splice(index, 1);
-    }
+const removeElement = (array, ...items) => {
+    items.forEach(item => {
+        const index = array.indexOf(item);
+        if (index !== -1)
+        {
+            array.splice(index, 1);
+        }
+    });
 }
 let array = ['Kiev', 'Beijing', 'Lima', 'Saratov'];
 removeElement(array, 'Lima');
