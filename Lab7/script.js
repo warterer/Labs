cb5f05177905bc9a268e385bfb16042742906036
areutskyi <artemik.tamik@gmail.com>
2024-12-13 21:21:20 +0200

    unique func

--- Lab7/script.js
+++ Lab7/script.js
@@ -14,3 +14,11 @@ console.log(array);
 array = [1, 2, 3, 4, 5, 6, 7];
 removeElement(array, 5);
 console.log(array);
+
+const unique = (array) => {
+    return array.filter((item, index) => array.indexOf(item) === index);
+}
+let result = unique([2, 1, 1, 3, 2]);
+console.log(result);
+result = unique(['top', 'bottom', 'top', 'left']);
+console.log(result);
