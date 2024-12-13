c605c3d78c75972fef0cc921a9b7e72e0839282f
areutskyi <artemik.tamik@gmail.com>
2024-12-13 20:36:30 +0200

    removeElement func

---
+++ Lab7/script.js
@@ -0,0 +1,14 @@
+const removeElement = (array, item) => {
+    const index = array.indexOf(item);
+    if (index !== -1)
+    {
+        array.splice(index, 1);
+    }
+}
+let array = ['Kiev', 'Beijing', 'Lima', 'Saratov'];
+removeElement(array, 'Lima');
+removeElement(array, 'Berlin');
+console.log(array);
+array = [1, 2, 3, 4, 5, 6, 7];
+removeElement(array, 5);
+console.log(array);
